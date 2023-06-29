65730d30c3387c942fe121b35c78cd82bfe5f02a
Viktor Daróczi <viktor.daroczi@gmail.com>
2023-06-29 08:44:29 +0200

    Clean up migrations.

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,7 +1,5 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_create_table;
-mod m20230626_000001_drop_table;
 mod m20230627_000001_create_user_table;
 mod m20230627_000002_create_project_table;
 mod m20230627_000003_create_ticket_table;
@@ -14,8 +12,6 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20220101_000001_create_table::Migration),
-            Box::new(m20230626_000001_drop_table::Migration),
             Box::new(m20230627_000001_create_user_table::Migration),
             Box::new(m20230627_000002_create_project_table::Migration),
             Box::new(m20230627_000003_create_ticket_table::Migration),
