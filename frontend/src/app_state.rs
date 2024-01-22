444cfacebb868ebf38e3dd48c9fac8c0823737a0
Viktor Daróczi <viktor.daroczi@gmail.com>
2024-01-22 08:23:57 +0100

    Apply more idiomatic reducer usage.

--- frontend/src/app_state.rs
+++ frontend/src/app_state.rs
@@ -16,66 +16,83 @@ pub struct AppState {
 
 impl AppState {
     pub fn update_dialog(ctx: &AppStateContext, dialog: Rc<Dialog>) {
-        ctx.dispatch(AppState {
-            dialog: dialog.clone(),
-            navbar_active: ctx.navbar_active,
-            identity: ctx.identity.clone(),
-            referer: ctx.referer.clone(),
-        });
+        ctx.dispatch(AppStateChange::UpdateDialog(dialog));
     }
 
     pub fn close_dialog(ctx: &AppStateContext) {
-        ctx.dispatch(AppState {
-            // This resets the dialog (which is closed by default):
-            dialog: Dialog::default().into(),
-            navbar_active: ctx.navbar_active,
-            identity: ctx.identity.clone(),
-            referer: ctx.referer.clone(),
-        });
+        ctx.dispatch(AppStateChange::CloseDialog);
     }
 
     pub fn toggle_navbar(ctx: &AppStateContext) {
-        ctx.dispatch(AppState {
-            dialog: ctx.dialog.clone(),
-            navbar_active: !ctx.navbar_active,
-            identity: ctx.identity.clone(),
-            referer: ctx.referer.clone(),
-        });
+        ctx.dispatch(AppStateChange::ToggleNavbar);
     }
 
     pub fn update_identity(ctx: &AppStateContext, identity: Option<LoginDto>) {
-        ctx.dispatch(AppState {
-            dialog: ctx.dialog.clone(),
-            navbar_active: ctx.navbar_active,
-            identity: identity.clone(),
-            referer: ctx.referer.clone(),
-        });
+        ctx.dispatch(AppStateChange::UpdateIdentity(identity));
     }
 
     pub fn update_identity_and_close_dialog(ctx: &AppStateContext, identity: Option<LoginDto>) {
-        ctx.dispatch(AppState {
-            dialog: Dialog::default().into(),
-            navbar_active: ctx.navbar_active,
-            identity: identity.clone(),
-            referer: ctx.referer.clone(),
-        });
+        ctx.dispatch(AppStateChange::UpdateIdentityAndCloseDialog(identity));
     }
 
     pub fn update_referer(ctx: &AppStateContext, referer: Option<Route>) {
-        ctx.dispatch(AppState {
-            dialog: ctx.dialog.clone(),
-            navbar_active: ctx.navbar_active,
-            identity: ctx.identity.clone(),
-            referer: referer.clone(),
-        });
+        ctx.dispatch(AppStateChange::UpdateReferer(referer));
     }
 }
 
+pub enum AppStateChange {
+    UpdateDialog(Rc<Dialog>),
+    CloseDialog,
+    ToggleNavbar,
+    UpdateIdentity(Option<LoginDto>),
+    UpdateIdentityAndCloseDialog(Option<LoginDto>),
+    UpdateReferer(Option<Route>),
+}
+
 impl Reducible for AppState {
-    type Action = AppState;
+    type Action = AppStateChange;
 
     fn reduce(self: Rc<Self>, action: Self::Action) -> Rc<Self> {
-        action.clone().into()
+        match action {
+            AppStateChange::UpdateDialog(dialog) => AppState {
+                dialog: dialog.clone(),
+                navbar_active: self.navbar_active,
+                identity: self.identity.clone(),
+                referer: self.referer.clone(),
+            },
+            AppStateChange::CloseDialog => AppState {
+                // This resets the dialog (which is closed by default):
+                dialog: Dialog::default().into(),
+                navbar_active: self.navbar_active,
+                identity: self.identity.clone(),
+                referer: self.referer.clone(),
+            },
+            AppStateChange::ToggleNavbar => AppState {
+                dialog: self.dialog.clone(),
+                navbar_active: !self.navbar_active,
+                identity: self.identity.clone(),
+                referer: self.referer.clone(),
+            },
+            AppStateChange::UpdateIdentity(identity) => AppState {
+                dialog: self.dialog.clone(),
+                navbar_active: self.navbar_active,
+                identity: identity.clone(),
+                referer: self.referer.clone(),
+            },
+            AppStateChange::UpdateIdentityAndCloseDialog(identity) => AppState {
+                dialog: Dialog::default().into(),
+                navbar_active: self.navbar_active,
+                identity: identity.clone(),
+                referer: self.referer.clone(),
+            },
+            AppStateChange::UpdateReferer(referer) => AppState {
+                dialog: self.dialog.clone(),
+                navbar_active: self.navbar_active,
+                identity: self.identity.clone(),
+                referer: referer.clone(),
+            },
+        }
+        .into()
     }
 }
 
