5fc483f923f46790389e547b12d85853042b97ca
Viktor Daróczi <viktor.daroczi@gmail.com>
2024-03-18 14:24:19 +0100

    Add default test user.

--- extra_migrations/mariadb/init.sql
+++ extra_migrations/mariadb/init.sql
@@ -1,2 +1,4 @@
 CREATE DATABASE IF NOT EXISTS ticketing;
 GRANT ALL PRIVILEGES ON ticketing.* TO ticketing;
+USE ticketing;
+INSERT INTO `users` VALUES (1,'Test Elek','password','Developer','test@example.com',0x4A9D3F3ABB8411EE81A900155DEE8A83);
