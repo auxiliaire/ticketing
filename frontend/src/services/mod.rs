e93b50b9bca929482dd08e793b6d3e250a2b5af9
Viktor Daróczi <viktor.daroczi@1und1.de>
2025-12-03 19:26:52 +0100

    Fix backend URL.

--- frontend/src/services/mod.rs
+++ frontend/src/services/mod.rs
@@ -3,8 +3,6 @@ pub mod project_service;
 pub mod ticket_service;
 pub mod user_service;
 
-const SERVER_URL: &str = env!("SERVER_URL");
-
 pub fn get_api_url() -> String {
-    format!("{}/", SERVER_URL)
+    String::from("/backend/")
 }
