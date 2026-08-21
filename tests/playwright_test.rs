1b8a81392d723678e3c03f784554c430cbd912f7
Viktor Daróczi <viktor.daroczi@gmail.com>
2026-08-21 16:28:48 +0200

    Fix playwright test.

--- tests/playwright_test.rs
+++ tests/playwright_test.rs
@@ -1,22 +1,41 @@
-use playwright::Playwright;
+use playwright_rs::{
+    protocol::MouseButton, BrowserContextOptions, ClickOptions, GotoOptions, LaunchOptions,
+    Playwright, Position, WaitUntil,
+};
 
 mod common;
 
-async fn test_with_playwright() -> Result<String, playwright::Error> {
-    let playwright = Playwright::initialize().await?;
-    playwright.prepare()?; // Install browsers
+async fn test_with_playwright() -> Result<String, playwright_rs::Error> {
+    let playwright = Playwright::launch().await?;
     let chromium = playwright.chromium();
-    let browser = chromium.launcher().headless(true).launch().await?;
-    let context = browser.context_builder().build().await?;
+    let launch_opts = LaunchOptions::new().headless(false);
+    let browser = chromium.launch_with_options(launch_opts).await?;
+    let ctx_opts = BrowserContextOptions::builder()
+        .user_agent(String::from("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36"))
+        .bypass_csp(true)
+        .build();
+    let context = browser.new_context_with_options(ctx_opts).await?;
     let page = context.new_page().await?;
-    page.goto_builder("https://playwright.dev/").goto().await?;
+    page.goto("https://playwright.dev/", None).await?;
 
     // Exec in browser and Deserialize with serde
-    let s: String = page.eval("() => location.href").await?;
+    let s: String = page.evaluate("() => location.href", None::<&()>).await?;
     assert_eq!(s, "https://playwright.dev/");
-    page.click_builder("a.getStarted_Sjon").click().await?;
+    let click_opts = ClickOptions::builder()
+        .button(MouseButton::Left)
+        .delay(190.0)
+        .position(Position { x: 7.0, y: 5.0 })
+        .build();
+    page.locator("a.getStarted_Sjon")
+        .click(Some(click_opts))
+        .await?;
 
-    page.wait_for_timeout(2000f64).await;
+    let goto_opts = GotoOptions::new();
+    page.wait_for_url(
+        "https://playwright.dev/docs/intro",
+        Some(goto_opts.wait_until(WaitUntil::DomContentLoaded)),
+    )
+    .await?;
 
     let title = page.title().await?;
 
@@ -24,6 +43,7 @@ async fn test_with_playwright() -> Result<String, playwright::Error> {
 }
 
 #[test]
+#[ignore = "for manual testing only"]
 fn test_page_title() {
     common::setup();
     tokio::runtime::Builder::new_multi_thread()
